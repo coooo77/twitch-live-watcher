bf89039965c1886fdad7d8076997637f11ee0c45
tzu lin chang <cosevenseven@gmail.com>
2022-07-11 01:19:25 +0800

    [Add] pinia

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
+import { createPinia } from 'pinia'
 // import './samples/node-api'
 
+const pinia = createPinia()
+
 createApp(App)
+  .use(pinia)
   .use(router)
   .mount('#app')
   .$nextTick(() => {
