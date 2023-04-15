52d5dbb6d0e7f2625c3eab3bfc57602c1b5a36e3
william chang <coooo77@hotmail.comcls>
2023-04-15 11:46:18 +0800

    fix: config layout

--- src/components/InputRow.vue
+++ src/components/InputRow.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="inputRow grid grid-cols-[150px,1fr,min-content] items-center gap-2">
-    <div class="inputTitle text-right font-bold text-themeColor4">
+  <div
+    class="inputRow grid grid-rows-2 grid-cols-[1fr,min-content] items-center gap-y-1 gap-x-2"
+  >
+    <div
+      class="inputTitle flex items-center gap-2 font-bold text-themeColor4 col-span-2"
+    >
+      <div class="h-2 w-2 bg-themeColor4 transform rotate-45 ml-1" />
       {{ title }}
     </div>
 
